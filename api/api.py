05f328321a46d6aa8db48117988316f86f081381
Ethan Holland <ethanjholland@gmail.com>
2018-07-28 21:15:23 +0000

    attempts to fix cors problems

--- api/api.py
+++ api/api.py
@@ -1,17 +1,18 @@
 import re
 import json
 import subprocess
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flask import Flask, request
 
-app = Flask(__name__)
+application = Flask(__name__)
 #app.config['DEBUG'] = True #automatically refresh on changes
-# CORS(app) #enable CORS for all use cases
-CORS(app, resources={r"*": {"origins": "*"}}) 
+CORS(application) #enable CORS for all use cases
+#CORS(application, resources={r"*": {"origins": "*"}}) 
 
 articlesFile='articles.txt'
 
-@app.route('/wikipediatracker', methods=['GET'])
+@application.route('/wikipediatracker', methods=['GET','POST'])
+@cross_origin()
 def getArticles():
     if 'n' in request.args:
         n=request.args['n']
@@ -21,17 +22,18 @@ def getArticles():
     s=subprocess.check_output(['tail','-n',str(n),articlesFile]).decode('utf-8')
     return json.dumps(re.split('\r?\n',s))
 
-@app.route('/wikipediatracker', methods=['POST'])
-def addArticle():
-    '''
-    append new article to end of file
-    expects json body of the form {"article":"Malcolm_Gladwell"}
-    '''
-    with open(articlesFile,'a') as f:
-        f.write('\n'+request.get_json()['article'])
-
-    return json.dumps({'success': True})
+#@application.route('/wikipediatracker', methods=['POST'])
+#@cross_origin()
+#def addArticle():
+#    '''
+#    append new article to end of file
+#    expects json body of the form {"article":"Malcolm_Gladwell"}
+#    '''
+#    with open(articlesFile,'a') as f:
+#        f.write('\n'+request.get_json()['article'])
+#
+#    return json.dumps({'success': True})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')
 
